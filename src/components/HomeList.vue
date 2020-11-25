d01b978a14fc65d573ef31ac816087484ef7cc17
lckevin27 <kevin27.cl@gmail.com>
2020-11-25 14:21:13 +0800

    Improve home page style, add random factors to the positions of the names.

--- src/components/HomeList.vue
+++ src/components/HomeList.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="home-list-container">
     <ul class="list">
-      <li>传武大师名册</li>
-      <li><a href="#">浑元形意太极门 马保国</a></li>
-      <li><a href="#">雷公太极 雷雷</a></li>
-      <li><a href="#">澳大利亚太极宗师 陈小旺</a></li>
-      <li><a href="#">武林大会冠军 王占军</a></li>
-      <li><a href="#">马云师兄 王占海</a></li>
-      <li><a href="#">武林大会冠军 王占军</a></li>
-      <li><a href="#">咏春 于昌华、丁浩</a></li>
+      <li></li>
+      <li><a href="#">浑元形意太极门 <b>马保国</b></a></li>
+      <li><a href="#">雷公太极 <b>雷雷</b></a></li>
+      <li><a href="#">澳大利亚太极宗师 <b>陈小旺</b></a></li>
+      <li><a href="#">武林大会冠军 <b>王占军</b></a></li>
+      <li><a href="#">马云师兄 <b>王占海</b></a></li>
+      <li><a href="#">咏春 <b>于昌华、丁浩</b></a></li>
     </ul>
   </div>
 </template>
