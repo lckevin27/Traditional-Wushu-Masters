01192b8645c9b69c6252b7a0b05d824762f10661
lckevin27 <kevin27.cl@gmail.com>
2020-11-26 00:12:52 +0800

    Add lightning effects to home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <div class="lightning">
+      <div class="lightning-container"><img src="@/assets/lightning/bolt-1.png" alt="lightning image 1"></div>
+      <div class="lightning-container"><img src="@/assets/lightning/bolt-2.png" alt="lightning image 2"></div>
+      <div class="lightning-container"><img src="@/assets/lightning/bolt-3.png" alt="lightning image 3"></div>
+    </div>
     <HomeList />
   </div>
 </template>
