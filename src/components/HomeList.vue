98d6efc6e3f97be332924e98e516ab739fbabbe4
lckevin27 <kevin27.cl@gmail.com>
2020-11-27 20:54:06 +0800

    Remove hard codes for home page, add dynamic links.

--- src/components/HomeList.vue
+++ src/components/HomeList.vue
@@ -2,12 +2,15 @@
   <div id="home-list-container">
     <ul class="list">
       <li></li>
-      <li><a href="timeline/马保国">浑元形意太极门 <b>马保国</b></a></li>
-      <li><a href="timeline/雷雷">雷公太极 <b>雷雷</b></a></li>
-      <li><a href="timeline/陈小旺">澳大利亚太极宗师 <b>陈小旺</b></a></li>
-      <li><a href="timeline/王占军">武林大会冠军 <b>王占军</b></a></li>
-      <li><a href="timeline/王占海">马云师兄 <b>王占海</b></a></li>
-      <li><a href="timeline/于昌华丁浩">咏春 <b>于昌华、丁浩</b></a></li>
+      <li v-for="(master, index) in allMasters" v-bind:key="index"><a v-bind:href="`timeline/${index}`">{{ master.title }} <b>{{ index }}</b></a></li>
     </ul>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: mapGetters(['allMasters'])
+}
+</script>
