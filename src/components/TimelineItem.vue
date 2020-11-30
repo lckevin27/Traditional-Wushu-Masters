0f4cefaf0b0d63652acedf82df9291d7a2d38eb9
lckevin27 <kevin27.cl@gmail.com>
2020-11-30 21:31:59 +0800

    Add a responsive sidebar for timelines.

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,7 +1,7 @@
 <template>
     <li>
       <div class="timeline-content">
-        <h3 class="date">{{ tlc.date }}</h3>
+        <h3 v-bind:id='`${tlc.tlTitle}`' class="date">{{ tlc.date }}</h3>
         <h1>{{ tlc.tlTitle }}</h1>
         <p v-html="tlc.tlText"></p>
       </div>
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="timeline">
     <a href="/" class="backToHome">◀ 回到主页</a>
-    <Intro/>
+    <div class="scrolling-nav">
+      <ul>
+        <li><a href='#人物生平'>人物生平</a></li>
+        <li v-for="tlc in allMasters[$route.params.name].timelineContents" v-bind:key="tlc.tlTitle"><a v-bind:href='`#${tlc.tlTitle}`'>{{ tlc.tlTitle }}</a></li>
+      </ul>
+    </div>
+    <Intro id="人物生平"/>
     <TimelineSection/>
     <img class="background-img" v-bind:src="require(`@/assets/timeline-background/${$route.params.name}.png`)" v-bind:alt="`${$route.params.name}`">
   </div>
@@ -10,12 +16,63 @@
 <script>
 import Intro from '@/components/Intro.vue'
 import TimelineSection from '@/components/TimelineSection.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Timeline',
   components: {
     TimelineSection,
     Intro
-  }
+  },
+  methods: {
+    onScroll: function () {
+      this.y = window.pageYOffset
+      if (this.y > this.prevY) {
+        if (this.y > this.coordList[this.curNum]) {
+          this.unhighlight()
+          this.navList[this.curNum].style.color = '#FFF'
+          this.curNum = this.navList[this.curNum + 1] ? this.curNum + 1 : this.curNum
+        }
+      } else {
+        if (this.y < this.coordList[this.curNum] + 10) {
+          this.unhighlight()
+          this.navList[this.curNum].style.color = '#FFF'
+          this.curNum = this.navList[this.curNum - 1] ? this.curNum - 1 : this.curNum
+        }
+      }
+      this.prevY = window.pageYOffset
+    },
+    unhighlight: function () {
+      this.navList.forEach(item => {
+        item.style.color = '#999'
+      })
+    }
+  },
+  data: function () {
+    return {
+      y: window.pageYOffset,
+      prevY: window.pageYOffset,
+      coordList: [],
+      navList: [],
+      curNum: 0
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
+    this.coordList.push(document.getElementById('人物生平').getBoundingClientRect().top + window.scrollY)
+    const timelineItems = document.getElementsByClassName('timeline-content')
+    timelineItems.forEach(item => {
+      this.coordList.push(item.getBoundingClientRect().top + window.scrollY - 30)
+    })
+    this.navList = document.querySelectorAll('.scrolling-nav ul li a')
+    this.navList.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        if (this.curNum !== index) {
+          this.curNum = index
+        }
+      })
+    })
+  },
+  computed: mapGetters(['allMasters'])
 }
 </script>
