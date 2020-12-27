5139c54dceeaba7b3e3d9bf832cfc61d376f67e9
lckevin27 <kevin27.cl@gmail.com>
2020-12-27 11:24:17 +0800

    Complete timeline's responsive design.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,16 @@
   <div id="navbar-wrapper">
     <nav id="sidenav">
       <a href="javascript:void(0)" class="closebtn" v-on:click="closeNav()">&times;</a>
-      <router-link v-for="(master, index) in allMasters" v-bind:key="index" v-bind:to="`/timeline/${index}`">{{ index }}</router-link>
+      <router-link to="/">回到主页</router-link>
+      <router-link v-for="(master, index) in allMasters" v-bind:key="index" v-bind:to="`/timeline/${index}`" v-on:click="closeNav()">{{ master.title }} {{ index }}</router-link>
     </nav>
     <div class="navbar">
       <router-link to="/" class="back-to-home">◀ 回到主页</router-link>
-      <a href="javascript:void(0)" class="open-nav" v-on:click="openNav()">OPEN</a>
+      <a href="javascript:void(0)" class="open-nav" v-on:click="openNav()">
+        <div></div>
+        <div></div>
+        <div></div>
+      </a>
     </div>
   </div>
 </template>
@@ -20,6 +25,7 @@ export default {
   methods: {
     openNav () {
       document.getElementById('sidenav').style.width = '300px'
+      document.getElementById('sidenav').style.maxWidth = '70%'
     },
     closeNav () {
       document.getElementById('sidenav').style.width = '0px'
